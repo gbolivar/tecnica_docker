73aa67e3e6d7a522ece9a454d1484ca8e2ee76f0
Gregorio Jose Bolivar <elalconxvii@gmail.com>
2019-08-07 10:53:40 -0300

    Quitar las ip y colocar host virtuales

--- startServices.sh
+++ startServices.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 ### Ing. Gregorio Bolivar <elalconxvi@gmail.com>
-## Start Services ##
-docker network rm tecnico_rests_net
-docker network prune -f
-docker-compose -f networks.yml up net_tecnica
 
 # Clonar repo con carpeta de codigo
 rm -rf webroot
